723d0737ae6f7fcfd4b487ebb390d9018176a876
Brooke Gill <brokegill@gmail.com>
2025-03-10 15:00:45 -0700

    [1.2.10] Added auto-moving to mvn package

--- src/main/java/io/nadeshiko/nadeshiko/Nadeshiko.java
+++ src/main/java/io/nadeshiko/nadeshiko/Nadeshiko.java
@@ -49,7 +49,7 @@ public class Nadeshiko {
 	/**
 	 * nadeshiko version number
 	 */
-	public static String VERSION = "1.2.9";
+	public static String VERSION = "1.2.10";
 
 	/**
 	 * Setting fallbacks (if missing from config.json)
